b89b66230aecdf99699792256f8b2f054e9b8f2c
acp-proju <32454409+acp-proju@users.noreply.github.com>
2018-01-21 20:56:30 +0200

    vuforia

--- UnityProjects/TietotaloTest/Assets/Standard Assets/CrossPlatformInput/Scripts/MobileControlRig.cs
+++ UnityProjects/TietotaloTest/Assets/Standard Assets/CrossPlatformInput/Scripts/MobileControlRig.cs
@@ -5,7 +5,7 @@ using UnityEditor;
 using UnityEngine;
 
 
-namespace UnityStandardAssets.CrossPlatformInput
+namespace Standard_Assets.CrossPlatformInput.Scripts
 {
     [ExecuteInEditMode]
     public class MobileControlRig : MonoBehaviour
